4dc210daa969e8e31e332d368db5df7abfea7838
khalifardy <khalifardy.miqdarsah@gmail.com>
2024-09-01 06:03:39 +0700

    jantan besar movement part-2

--- utils/kma_method.py
+++ utils/kma_method.py
@@ -14,11 +14,12 @@ class KomodoMlipirAlgorithm:
     fitness_function : fungsi fitness yang akan dioptimalkan
     space_search : batasan pencarian
     max_iter : jumlah iterasi
-    info_katogorikal_tipe : tipe data kategorikal
+    random_state : state untuk keacakan
+    problem : masalah yang akan diselesaikan, "maximize" atau "minimize"
     
     """
     
-    def __init__(self, n:int, p:float, d:float, fitness_function:callable , space_search:dict, max_iter:int, info_katogorikal_tipe:list):
+    def __init__(self, n:int, p:float, d:float, fitness_function:callable , space_search:dict, max_iter:int, info_katogorikal_tipe:list, problem:str="maximize",random_state:int=22 ):
         self.n = n
         self.p = p
         self.d = d
@@ -34,6 +35,8 @@ class KomodoMlipirAlgorithm:
         self.jk_fitness = None
         self.betina_fitness = None
         self.kategorikal_index = []
+        self.random_state = random_state
+        self.problem = problem
         
     
     def jantan_besar(self):
@@ -52,14 +55,8 @@ class KomodoMlipirAlgorithm:
         for _ in range(self.n):
             individu_komodo = []
             while individu_komodo not in self.populasi_komodo:
-                count = 0
-                for key in self.space_search.keys():
-                    if key in self.info_katogorikal_tipe:
-                        individu_komodo.append(np.random.choice(self.space_search[key]))
-                        self.kategorikal_index.append(count)
-                    else:
-                        individu_komodo.append(np.random.uniform(self.space_search[key][0],self.space_search[key][1]))
-                    count += 1
+                individu_komodo = np.random.default_rng(self.random_state).random(len(self.space_search.keys()))
+        
                 if individu_komodo in self.populasi_komodo :
                     individu_komodo = []
                 else:
@@ -70,23 +67,10 @@ class KomodoMlipirAlgorithm:
         
         jumlah_jantan_besar = self.jantan_besar()
         
-        urutan = []
-        fitness_value = []
-        count = 0
-        
-        while len(urutan) != len(self.populasi_komodo):
-            cuurent = self.fitness_function(self.populasi_komodo[count])
-            idx = count
-            
-            if count not in urutan:
-                for i in len(self.populasi_komodo):
-                    if self.fitness_function(self.populasi_komodo[i]) > cuurent and i not in urutan and count != i:
-                        cuurent = self.fitness_function(self.populasi_komodo[i])
-                        idx = i
-                urutan.append(idx)
-                fitness_value.append(cuurent)
-            else:
-                count += 1
+        if self.problem == "maximize":
+            urutan, fitness_value = self.maximize_problem()
+        else:
+            urutan, fitness_value = self.minimize_problem()
                     
         
         self.jb = urutan[:jumlah_jantan_besar]
@@ -96,45 +80,86 @@ class KomodoMlipirAlgorithm:
         self.jb_fitness = fitness_value[:jumlah_jantan_besar]
         self.betina_fitness = fitness_value[jumlah_jantan_besar]
         self.jk_fitness = fitness_value[jumlah_jantan_besar+1:]
-        
-                   
     
     def movement_jantan_besar(self):
-        # fungsi untuk pergerakan jantan besar
-        new_posisi = []
+        #fungsi untuk movement jantan besar
+        individu_baru = []
         for i in range(len(self.jb)):
-            individu1 = self.populasi_komodo[self.jb[i]]
-            new_individu = []
-            w = []
-            for j in range(len(self.jb)):
-                
+            individu1 = self.jb[i]
+            w = [] 
+            for j in range(len(self.populasi_komodo[self.jb[i]])):
                 if i != j:
                     r1 = self.random_normal_0_1()
                     r2 = self.random_normal_0_1()
-                    individu2 = self.populasi_komodo[self.jb[j]]
-                    if self.jb_fitness[i]>= self.jb_fitness[j] and r2 < 0.5:
-                        for l in range(len(individu1)):
-                            if l not in self.kategorikal_index:
-                                w.append(individu1[l] + r1*(individu2[l] - individu1[l]))
-                            #else:
+                    individu2 = self.jb[j]
+                
+                    if self.jb_fitness[j] > self.jb_fitness[i] or r2 < 0.5:
+                        w.append(r1*(individu2-individu1))
+                    else:
+                        w.append(r1*(individu1-individu2))
+                    individu_baru.append(individu1 + sum(w))
+        
+        return individu_baru
+                    
+                        
+        
+    def movement_betina(self):
+        #fungsi untuk update betina
+        
+        pass
                                 
                         
                         
-    
-    def interpolasi_kategorikal(self, individu1, individu2,r1):
-        #fungsi interpolasi untuk dimensi kategorikal (eksploitasi)
-        return round(r1 *individu2 + (1-r1)*individu1)
-    
-    #def probablilitas_katogrikal(self,fitnes_individu1, fitnes_individu2,beta):
-        #fungsi probabilitas untuk dimensi kategorikal (eksplorasi)
-        #penyebut = 
+    def maximize_problem(self):
+        #mengurutkan dari terbesar ke terkecil
         
+        urutan = []
+        fitness_value = []
+        count = 0
         
+        while len(urutan) != len(self.populasi_komodo):
+            cuurent = self.fitness_function(self.populasi_komodo[count])
+            idx = count
+            
+            if count not in urutan:
+                for i in range(len(self.populasi_komodo)):
+                    if self.fitness_function(self.populasi_komodo[i]) >= cuurent and i not in urutan and count != i:
+                        cuurent = self.fitness_function(self.populasi_komodo[i])
+                        idx = i
+                urutan.append(idx)
+                fitness_value.append(cuurent)
+            else:
+                count += 1
+        
+        return urutan, fitness_value
+    
+    def minimize_problem(self):
+        #mengurutkan dari terkecil ke terbesar
+        
+        urutan = []
+        fitness_value = []
+        count = 0
+        
+        while len(urutan) != len(self.populasi_komodo):
+            cuurent = self.fitness_function(self.populasi_komodo[count])
+            idx = count
+            
+            if count not in urutan:
+                for i in range(len(self.populasi_komodo)):
+                    if self.fitness_function(self.populasi_komodo[i]) <= cuurent and i not in urutan and count != i:
+                        cuurent = self.fitness_function(self.populasi_komodo[i])
+                        idx = i
+                urutan.append(idx)
+                fitness_value.append(cuurent)
+            else:
+                count += 1
+        
+        return urutan, fitness_value
     
     def random_normal_0_1(self):
         # fungsi untuk mendapatkan nilai random dari distribusi normal
         
         while True:
-            value = np.random.normal(0.5,0.1)
+            value = np.random.default_rng(self.random_state).normal(0.5,0.1)
             if 0 <=value<=1:
                 return value
