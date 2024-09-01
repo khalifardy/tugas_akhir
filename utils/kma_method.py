ea01d23be9b57aa7a90853409b942bfb67e38136
khalifardy <khalifardy.miqdarsah@gmail.com>
2024-09-01 22:15:39 +0700

    betina movement part - 1

--- utils/kma_method.py
+++ utils/kma_method.py
@@ -16,11 +16,12 @@ class KomodoMlipirAlgorithm:
     max_iter : jumlah iterasi
     random_state : state untuk keacakan
     problem : masalah yang akan diselesaikan, "maximize" atau "minimize"
+    alpha : radius parthenogenesis
     
     """
     
-    def __init__(self, n:int, p:float, d:float, fitness_function:callable , space_search:dict, max_iter:int, info_katogorikal_tipe:list, problem:str="maximize",random_state:int=22 ):
-        self.n = n
+    def __init__(self, n:int, p:float, d:float, fitness_function:callable , space_search:dict, max_iter:int, info_katogorikal_tipe:list, problem:str="maximize",random_state:int=22, alpha:float = 0.1 ):
+        self.n = n 
         self.p = p
         self.d = d
         self.fitness_function = fitness_function
@@ -37,6 +38,7 @@ class KomodoMlipirAlgorithm:
         self.kategorikal_index = []
         self.random_state = random_state
         self.problem = problem
+        self.alpha = alpha
         
     
     def jantan_besar(self):
@@ -100,13 +102,34 @@ class KomodoMlipirAlgorithm:
                     individu_baru.append(individu1 + sum(w))
         
         return individu_baru
-                    
+        
+             
                         
         
     def movement_betina(self):
         #fungsi untuk update betina
         
-        pass
+        prob = self.random_normal_0_1()
+        r1 = self.random_normal_0_1()
+        
+        if prob < 0.5 : 
+            anak1 = r1 * self.betina  + (1 - r1) * self.jb[0]
+            anak2 = r1 * self.jb[0] + (1-r1) * self.betina
+            
+            fitnes1 = self.fitness_function(anak1)
+            fitnes2 = self.fitness_function(anak2)
+            
+            if fitnes1 > fitnes2:
+                return anak1
+            else:
+                return anak2
+            
+        else:
+            return self.betina + (2*r1 - 1) * self.alpha
+        
+        
+        
+        
                                 
                         
                         
